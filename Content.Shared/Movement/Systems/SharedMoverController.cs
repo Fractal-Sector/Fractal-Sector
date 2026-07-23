ea9acd710dabd15825b4da7bd655eeee7538c0ce
IsNotAcceptable <184727191+IsNotAcceptable@users.noreply.github.com>
2026-07-23 17:29:09 +0700

    ?

--- Content.Shared/Movement/Systems/SharedMoverController.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -352,8 +352,7 @@ public abstract partial class SharedMoverController : VirtualController
             if (!weightless && MobMoverQuery.TryGetComponent(uid, out var mobMover) &&
                 TryGetSound(weightless, uid, mover, mobMover, xform, out var sound, tileDef: tileDef))
             {
-                var soundModifier = mover.Sprinting ? InputMoverComponent.SprintingSoundModifier
-                    : InputMoverComponent.WalkingSoundModifier;
+                var soundModifier = mover.Sprinting ? 3.5f : 1.5f;
 
                 var audioParams = sound.Params
                     .WithVolume(sound.Params.Volume + soundModifier)
@@ -679,11 +678,10 @@ public abstract partial class SharedMoverController : VirtualController
 
     private void OnTileFriction(Entity<MovementSpeedModifierComponent> ent, ref TileFrictionEvent args)
     {
-        if (!PhysicsQuery.TryComp(ent, out var physicsComponent))
+        if (!TryComp<PhysicsComponent>(ent, out var physicsComponent) || !XformQuery.TryComp(ent, out var xform))
             return;
 
-        // TODO: Make IsWeightless event based!!!
-        if (physicsComponent.BodyStatus != BodyStatus.OnGround || _gravity.IsWeightless(ent, physicsComponent, Transform(ent)))
+        if (physicsComponent.BodyStatus != BodyStatus.OnGround || _gravity.IsWeightless(ent.Owner))
             args.Modifier *= ent.Comp.BaseWeightlessFriction;
         else
             args.Modifier *= ent.Comp.BaseFriction;
--- Content.Shared/Weapons/Melee/MeleeThrowOnHitSystem.cs
+++ Content.Shared/Weapons/Melee/MeleeThrowOnHitSystem.cs
@@ -1,52 +1,33 @@
+using Content.Shared.Construction.Components;
 using Content.Shared.Stunnable;
 using Content.Shared.Throwing;
 using Content.Shared.Timing;
 using Content.Shared.Weapons.Melee.Components;
 using Content.Shared.Weapons.Melee.Events;
-using Robust.Shared.Physics.Components;
-using System.Numerics;
 using Content.Shared.Whitelist; // Frontier
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Systems;
+using System.Numerics;
 
 namespace Content.Shared.Weapons.Melee;
 
 /// <summary>
 /// This handles <see cref="MeleeThrowOnHitComponent"/>
 /// </summary>
-public sealed class MeleeThrowOnHitSystem : EntitySystem
+public sealed partial class MeleeThrowOnHitSystem : EntitySystem
 {
-    [Dependency] private readonly SharedTransformSystem _transform = default!;
-    [Dependency] private readonly UseDelaySystem _delay = default!;
-    [Dependency] private readonly SharedStunSystem _stun = default!;
-    [Dependency] private readonly ThrowingSystem _throwing = default!;
-    [Dependency] private readonly EntityWhitelistSystem _whitelist = default!; // Frontier
+    [Dependency] private SharedTransformSystem _transform = default!;
+    [Dependency] private SharedPhysicsSystem _physics = default!;
+    [Dependency] private UseDelaySystem _delay = default!;
+    [Dependency] private SharedStunSystem _stun = default!;
+    [Dependency] private ThrowingSystem _throwing = default!;
+    [Dependency] private EntityWhitelistSystem _whitelist = default!; // Frontier
     /// <inheritdoc/>
     public override void Initialize()
     {
         SubscribeLocalEvent<MeleeThrowOnHitComponent, MeleeHitEvent>(OnMeleeHit);
         SubscribeLocalEvent<MeleeThrowOnHitComponent, ThrowDoHitEvent>(OnThrowHit);
-        SubscribeLocalEvent<MeleeThrowOnHitComponent, ThrownEvent>(OnThrow);
-        SubscribeLocalEvent<MeleeThrowOnHitComponent, LandEvent>(OnLand);
-    }
-
-    private void OnThrow(Entity<MeleeThrowOnHitComponent> ent, ref ThrownEvent args)
-    {
-        if (_delay.IsDelayed(ent.Owner))
-            return;
-
-        ent.Comp.HitWhileThrown = false;
-        ent.Comp.ThrowOnCooldown = false;
-
-        DirtyField(ent, ent.Comp, nameof(MeleeThrowOnHitComponent.HitWhileThrown));
-        DirtyField(ent, ent.Comp, nameof(MeleeThrowOnHitComponent.ThrowOnCooldown));
-    }
-
-    private void OnLand(Entity<MeleeThrowOnHitComponent> ent, ref LandEvent args)
-    {
-        if (ent.Comp.HitWhileThrown && !_delay.IsDelayed(ent.Owner))
-            _delay.TryResetDelay(ent.Owner);
-
-        ent.Comp.ThrowOnCooldown = true;
-        DirtyField(ent, ent.Comp, nameof(MeleeThrowOnHitComponent.ThrowOnCooldown));
     }
 
     private void OnMeleeHit(Entity<MeleeThrowOnHitComponent> weapon, ref MeleeHitEvent args)
@@ -75,14 +56,8 @@ public sealed class MeleeThrowOnHitSystem : EntitySystem
         if (!weapon.Comp.ActivateOnThrown)
             return;
 
-        if (weapon.Comp.ThrowOnCooldown)
-            return;
-
         if (!TryComp<PhysicsComponent>(args.Thrown, out var weaponPhysics))
             return;
-
-        weapon.Comp.HitWhileThrown = true;
-        DirtyField(weapon, weapon.Comp, nameof(MeleeThrowOnHitComponent.HitWhileThrown));
 
         ThrowOnHitHelper(weapon, args.Component.Thrower, args.Target, weaponPhysics.LinearVelocity);
     }
@@ -99,15 +74,11 @@ public sealed class MeleeThrowOnHitSystem : EntitySystem
         RaiseLocalEvent(target, ref startEvent);
 
         if (ent.Comp.StunTime != null)
-            _stun.TryAddParalyzeDuration(target, ent.Comp.StunTime.Value);
+            _stun.TryParalyze(target, ent.Comp.StunTime.Value, false);
 
         if (direction == Vector2.Zero)
             return;
 
-        // Frontier: check that hit entity passes whitelist
-        var unanchorOnHit = ent.Comp.UnanchorOnHit && _whitelist.IsWhitelistPassOrNull(ent.Comp.Whitelist, target);
-        // End Frontier
-
-        _throwing.TryThrow(target, direction.Normalized() * ent.Comp.Distance, ent.Comp.Speed, user, unanchor: unanchorOnHit); // Frontier: ent.Comp.UnanchorOnHit<unanchorOnHit
+        _throwing.TryThrow(target, direction.Normalized() * ent.Comp.Distance, ent.Comp.Speed, user, unanchor: ent.Comp.UnanchorOnHit);
     }
 }
