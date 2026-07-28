dd772ea4f89d121a38c1d4f647c0aacc0c6aeda1
IsNotAcceptable <184727191+IsNotAcceptable@users.noreply.github.com>
2026-07-28 23:04:00 +0700

    ?

--- Content.Server/NPC/NPCBlackboard.cs
+++ Content.Server/NPC/NPCBlackboard.cs
@@ -20,22 +20,22 @@ public sealed partial class NPCBlackboard : IEnumerable<KeyValuePair<string, obj
     {
         {"BufferRange", 7f}, // FS: 10<7
         {"FollowCloseRange", 3f},
-        {"FollowRange", 6f}, // FS: 7<6
-        {"IdleRange", 6f}, // FS: 7<6
+        {"FollowRange", 7f},
+        {"IdleRange", 7f},
         {"InteractRange", SharedInteractionSystem.InteractionRange},
         {"LightbotRange", 32f}, // Wayfarer
         {"MaximumIdleTime", 7f},
         {MedibotInjectRange, 4f},
         {MeleeMissChance, 0.3f},
         {"MeleeRange", 1f},
-        {"MinimumIdleTime", 3f}, // FS: 2<3
+        {"MinimumIdleTime", 2f},
         {"MovementRangeClose", 0.2f},
         {"MovementRange", 1.5f},
         {"RangedRange", 7f}, // FS: 10<7
         {"RotateSpeed", float.MaxValue},
         // #Misfits Change — slightly widen default detection bands so hostiles start reacting a bit earlier at player view edges.
-        {"VisionRadius", 7f}, // FS: 14<7
-        {"AggroVisionRadius", 7f}, // FS: 14<7
+        {"VisionRadius", 14f},
+        {"AggroVisionRadius", 9f}, // FS: 7<9
         {"TurretRange", 14f}, // Wayfarer // FS: 20<14
     };
 
